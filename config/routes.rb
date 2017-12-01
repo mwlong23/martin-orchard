613b51efe08e27fad092a86328cd0342dd1886a5
Mitchell <meechllada@gmail.com>
2017-12-01 09:58:20 -0800

    create devise register and login views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :products
+  devise_for :users
+
+
+  resources :products do
+    resources :reviews
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171201173230) do
+ActiveRecord::Schema.define(version: 20171201174706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "products", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.integer "weight"
-    t.integer "box_price"
-    t.integer "weekly_deliver_cost"
+  create_table "products", id: :bigserial, force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.integer  "weight"
+    t.integer  "box_price"
+    t.integer  "weekly_deliver_cost"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+  end
+
+  create_table "reviews", id: :bigserial, force: :cascade do |t|
+    t.string   "title"
+    t.string   "body"
+    t.integer  "rating"
+    t.integer  "product_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "reviews", force: :cascade do |t|
-    t.string "title"
-    t.string "body"
-    t.integer "rating"
-    t.integer "product_id"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
